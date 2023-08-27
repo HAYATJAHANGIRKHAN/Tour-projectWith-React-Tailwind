7dcd813bd9b6fea15d0d105580f51cc6ee823f86
Hayat Jahangir Khan <112794077+HAYATJAHANGIRKHAN@users.noreply.github.com>
2023-08-27 12:17:41 +0530

    Add files via upload
    upload tour datails

--- src/Component/Tours.jsx
+++ src/Component/Tours.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react'
+import Card from './Card';
+const Tours = (props) => {
 
+    function removeHandler(id) {
+        console.log(id);
+        props.removeTours(id);
+    }
 
-export default function Tours({tours}) {
-
-   
-  return (
-    <div>
-      <div>
-      <h1> Kashmir Tourism</h1>
-      </div> 
-      <div>
-          {
-            tours.map( (tours) =>{
-                return <card {...tours}></card>
-            })
-          }
-      </div>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div>
+                <h2 className='title'>Plan with Love</h2>
+            </div>
+            <div className='cards'>
+                {
+                    props.tours.map((tour) => {
+                        return <Card {...tour} key={tour.id} removeTour={removeHandler} />;
+                    })
+                }
+            </div>
+        </div>
+    )
 }
+
+export default Tours;
