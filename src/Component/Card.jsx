e5795c2bae5e510c830971edb0f54a3ea5cb5523
Hayat Jahangir Khan <112794077+HAYATJAHANGIRKHAN@users.noreply.github.com>
2023-08-27 12:26:24 +0530

    Add files via upload
    cards upgrade

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,34 +1,35 @@
-import React, { useState } from "react";
-
-const Card = ({ id, image, info, price, name, removeTour }) => {
-    const [readmore, setReadMore] = useState(false);
-    const description = readmore ? info : `${info.substring(0, 200)}....`;
-
-    function readmoreHandler() {
-        setReadMore(!readmore);
-    }
+import  { useState } from "react";
 
 
-    return (
-        <div className="card">
-            <img src={image} className="image" alt="img"/>
-            <div className="tourInfo">
-                <div className="tourDetails">
-                    <h4 className="tourPrice">{price}</h4>
-                    <h4 className="tourName">{name}</h4>
-                </div>
-                <div className="description">
-                    {description}
-                    <span className="readMore" onClick={readmoreHandler}>
-                        {readmore ? `show less` : `read more`}
-                    </span>
-                </div>
-            </div>
-            <button className="btnRed" onclick={() => removeTour(id)}>
-                Not Interested
-            </button>
-        </div >
-    );
-};
 
+function Card({id, image, info, price, name,removeTour}) {
+  const [readmore, setReadmore] = useState(false);
+  const description =  readmore ? info :`${info.substring(0, 200)}....`;
+
+  function readmoreHandler() {
+    setReadmore(!readmore);
+  }
+  
+
+  return (
+    <div className="card">
+      <img src={image} className="image" ></img>
+      <div className="tour-info">
+        <div className="tour-details">
+          <h4 className="tour-price">{price}</h4>
+          <h4 className="tour-name">{name}</h4>
+        </div>
+        <div className="description">
+          {description}
+          <span className="read-more" onClick={readmoreHandler}>
+            {readmore ? `show less` : `read more`}
+          </span>
+        </div>
+      </div>
+
+      <button className="btn-red" onClick={() => removeTour(id)}>Not Interested</button>
+
+    </div>
+  );
+}
 export default Card;
