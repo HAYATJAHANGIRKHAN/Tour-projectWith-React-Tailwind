d67e7928531ba66d51bf60740392eae6cecd1d54
Hayat Jahangir Khan <112794077+HAYATJAHANGIRKHAN@users.noreply.github.com>
2023-08-27 12:20:14 +0530

    Add files via upload
    update data about tourist places

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Tours from './Component/Tours';
 const App = () => {
   const [tours, setTours] = useState(data);
   
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
 
-
+  if (tours.length === 0) {
     return (
       <div className="refresh">
-        <Tours tours={tours}></Tours>
+        <h2>No Tours Left</h2>
+        <button className="btnWhite" onClick={() => setTours(data)}>Refresh</button>
       </div>
     );
-  
+  }
 
+  return (
+    <div className="app">
+      <Tours tours={tours} removeTours={removeTour} />
+    </div>
+  )
 
 };
 
